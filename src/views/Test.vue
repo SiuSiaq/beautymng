47fb5642b113808588068a60d1def95ca0f6a4a1
SiuSiaq <siudak.maciej01@gmail.com>
2020-12-26 02:56:42 +0100

    home tabs

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,6 +1,230 @@
 <template>
   <v-container fluid class="testHomeClass">
-    <v-row>
+    <div v-if="$vuetify.breakpoint.mobile" style="height: 90vh;" class="pb-10">
+      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+        <v-tab v-for="item in items" :key="item">
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card
+            max-height="84vh"
+            v-if="getUserData.doctor"
+            elevation="12"
+            rounded="lg"
+            class="pa-3"
+            ><v-sheet height="64">
+              <v-toolbar flat color="white">
+                <v-btn
+                  outlined
+                  class="mr-4"
+                  color="grey darken-2"
+                  @click="setToday"
+                  >Dzisiaj</v-btn
+                >
+                <v-btn fab text small color="grey darken-2" @click="prev">
+                  <v-icon small>mdi-chevron-left</v-icon>
+                </v-btn>
+                <v-btn fab text small color="grey darken-2" @click="next">
+                  <v-icon small>mdi-chevron-right</v-icon>
+                </v-btn>
+                <v-toolbar-title v-if="$refs.calendar">{{
+                  $refs.calendar.title
+                }}</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+            </v-sheet>
+            <v-sheet height="490">
+              <v-calendar
+                locale="pl-PL"
+                ref="calendar"
+                v-model="focus"
+                color="primary"
+                :events="getMyEvents"
+                :event-color="getEventColor"
+                @click:event="showEvent"
+                type="day"
+              >
+                <template #day-body="{ date, week }">
+                  <div
+                    class="v-current-time"
+                    :class="{ first: date === week[0].date }"
+                    :style="{ top: nowY }"
+                  ></div>
+                </template>
+              </v-calendar>
+              <v-menu
+                v-model="selectedOpen"
+                :close-on-content-click="false"
+                :activator="selectedElement"
+                offset-x
+              >
+                <v-card color="grey lighten-4" max-width="350" flat>
+                  <v-toolbar :color="selectedEvent.color" dark>
+                    <v-toolbar-title>{{ selectedEvent.name }}</v-toolbar-title>
+                  </v-toolbar>
+                  <v-expansion-panels>
+                    <v-expansion-panel>
+                      <v-expansion-panel-header disable-icon-rotate>
+                        Klient: {{ selectedEvent.clientName }}
+                        <template v-slot:actions>
+                          <v-icon color="primary">mdi-account</v-icon>
+                        </template>
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <v-simple-table>
+                          <tbody>
+                            <tr>
+                              <td>Imię</td>
+                              <td>{{ selectedClient.name }}</td>
+                            </tr>
+                            <tr>
+                              <td>Nazwisko</td>
+                              <td>{{ selectedClient.surname }}</td>
+                            </tr>
+                            <tr>
+                              <td>Telefon</td>
+                              <td>
+                                <a :href="`tel:${selectedClient.phone}`">{{
+                                  selectedClient.phone
+                                }}</a>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Email</td>
+                              <td>
+                                <a :href="`mailto:${selectedClient.email}`">{{
+                                  selectedClient.email
+                                }}</a>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Wizyt</td>
+                              <td>{{ selectedClient.visits }}</td>
+                            </tr>
+                          </tbody>
+                        </v-simple-table>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+
+                    <v-expansion-panel>
+                      <v-expansion-panel-header disable-icon-rotate>
+                        Zabieg: {{ selectedTreatment.name }}
+                        <template v-slot:actions>
+                          <v-icon color="primary">mdi-needle</v-icon>
+                        </template>
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <v-simple-table>
+                          <tbody>
+                            <tr>
+                              <td>Zabieg</td>
+                              <td>{{ selectedTreatment.name }}</td>
+                            </tr>
+                            <tr>
+                              <td>Czas</td>
+                              <td>
+                                {{ selectedTreatment.hours }} godziny
+                                {{ selectedTreatment.minutes }} minuty
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Cena</td>
+                              <td>{{ selectedTreatment.price }}zł</td>
+                            </tr>
+                            <tr>
+                              <td>Wykonano</td>
+                              <td>{{ selectedTreatment.visits }}</td>
+                            </tr>
+                          </tbody>
+                        </v-simple-table>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                    <v-expansion-panel>
+                      <v-expansion-panel-header disable-icon-rotate>
+                        Status:<span
+                          class="ml-1"
+                          :class="
+                            selectedEvent.confirmed
+                              ? 'success--text'
+                              : 'error--text'
+                          "
+                        >
+                          {{
+                            selectedEvent.confirmed
+                              ? "Potwierdzono"
+                              : "Nie potwierdzono"
+                          }}</span
+                        >
+                        <template v-slot:actions>
+                          <v-icon
+                            :color="
+                              selectedEvent.confirmed ? 'success' : 'error'
+                            "
+                            >{{
+                              selectedEvent.confirmed
+                                ? "mdi-check"
+                                : "mdi-close-circle"
+                            }}</v-icon
+                          >
+                        </template>
+                      </v-expansion-panel-header>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                  <v-card-actions>
+                    <v-btn text color="black" @click="selectedOpen = false"
+                      >anuluj</v-btn
+                    >
+                    <v-spacer></v-spacer>
+                    <EventSummary
+                      :event="selectedEvent"
+                      @archived="selectedOpen = false"
+                      v-if="!selectedEvent.archived"
+                    />
+                  </v-card-actions>
+                </v-card>
+              </v-menu> </v-sheet
+          ></v-card>
+        </v-tab-item>
+
+        <v-tab-item>
+          <v-card class="pa-3" rounded="lg">
+            <v-card-title class="text-h4 grey--text text--darken-2"
+              >Dzisiaj</v-card-title
+            >
+            <v-list
+              v-if="getTodayNotConfirmedEvents.length > 0"
+              :class="!$vuetify.breakpoint.mobile ? 'desktop' : ''"
+              three-line
+            >
+              <v-list-item-group>
+                <ConfirmEventItem
+                  v-for="event in getTodayNotConfirmedEvents"
+                  :key="event.id"
+                  :event="event"
+                />
+              </v-list-item-group>
+            </v-list>
+            <div v-else class="d-flex">
+              <v-icon
+                :size="!$vuetify.breakpoint.mobile ? 50 : 30"
+                color="success"
+                class="fab mr-3"
+                >mdi-check-circle</v-icon
+              >
+              <div
+                class="mt-md-2 text-subtitle-1 font-weight-regular text-md-h5 success--text"
+              >
+                Wszystkie zabiegi potwierdzone
+              </div>
+            </div>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </div>
+    <v-row v-else>
       <v-col cols="12" md="4">
         <v-card
           max-height="84vh"
@@ -181,16 +405,20 @@
       </v-col>
       <v-col cols="12" md="4">
         <v-card class="pa-3" rounded="lg">
-          <v-card-title
-            class="text-h4 grey--text text--darken-2"
-          >Dzisiaj</v-card-title>
+          <v-card-title class="text-h4 grey--text text--darken-2"
+            >Dzisiaj</v-card-title
+          >
           <v-list
             v-if="getTodayNotConfirmedEvents.length > 0"
             :class="!$vuetify.breakpoint.mobile ? 'desktop' : ''"
             three-line
           >
             <v-list-item-group>
-              <ConfirmEventItem v-for="event in getTodayNotConfirmedEvents" :key="event.id" :event="event" />
+              <ConfirmEventItem
+                v-for="event in getTodayNotConfirmedEvents"
+                :key="event.id"
+                :event="event"
+              />
             </v-list-item-group>
           </v-list>
           <div v-else class="d-flex">
@@ -198,10 +426,13 @@
               :size="!$vuetify.breakpoint.mobile ? 50 : 30"
               color="success"
               class="fab mr-3"
-            >mdi-check-circle</v-icon>
+              >mdi-check-circle</v-icon
+            >
             <div
               class="mt-md-2 text-subtitle-1 font-weight-regular text-md-h5 success--text"
-            >Wszystkie zabiegi potwierdzone</div>
+            >
+              Wszystkie zabiegi potwierdzone
+            </div>
           </div>
         </v-card>
       </v-col>
@@ -223,6 +454,8 @@ export default {
     selectedClient: {},
     selectedElement: null,
     selectedOpen: false,
+    tab: null,
+    items: ["Dzisiaj", "Do potwierdzenia"],
   }),
   components: {
     EventSummary,
@@ -235,7 +468,7 @@ export default {
     this.todayNotConfirmedEvents();
   },
   methods: {
-    ...mapActions(["archiveEvent", 'todayNotConfirmedEvents']),
+    ...mapActions(["archiveEvent", "todayNotConfirmedEvents"]),
     getEventColor(event) {
       return !event.archived ? event.color : "#969696";
     },
@@ -288,7 +521,12 @@ export default {
     },
   },
   computed: {
-    ...mapGetters(["getAllEvents", "getSalon", "getUserData", 'getTodayNotConfirmedEvents']),
+    ...mapGetters([
+      "getAllEvents",
+      "getSalon",
+      "getUserData",
+      "getTodayNotConfirmedEvents",
+    ]),
     cal() {
       return this.ready ? this.$refs.calendar : null;
     },
